77d4793f805571bee78c65c56f6195708d1809a7
dianacamacho <dcam1901@gmail.com>
2017-07-05 23:52:35 -0500

    added event page, send and listen to messages from content script and event page and popup js to change color

--- content.js
+++ content.js
@@ -1,2 +1,9 @@
 // send message to event page js to highlight the extension icon, since content script cant access the chrom api itself
 chrome.runtime.sendMessage({todo: "showPageAction"});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.todo == "changeColor") {
+    var addColor = '#' + request.clickedColor;
+    $('.api').css('color', addColor);
+  }
+})
